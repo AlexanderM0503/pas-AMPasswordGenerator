9033bb9a13752182de65cc78ec15e76b09dd7089
Alexander Mulyar <mulyar.alexander0503@yandex.ru>
2024-10-22 15:46:58 +0500

    Create the MainForm

--- formmain.pas
+++ formmain.pas
@@ -5,10 +5,33 @@ unit FormMain;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, StdCtrls, Spin;
 
 type
-  TForm1 = class(TForm)
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    MainFormMenu: TMainMenu;
+    MainFormMenuFile: TMenuItem;
+    MainFormMenuDefaultSettings: TMenuItem;
+    MainFormMenuSep1: TMenuItem;
+    MainFormMenuExit: TMenuItem;
+    MainFormMenuHelp: TMenuItem;
+    MainFormMenuAbout: TMenuItem;
+    GroupNumChars: TGroupBox;
+    NumChars: TSpinEdit;
+    BtnSetNum4: TButton;
+    BtnSetNum8: TButton;
+    BtnSetNum12: TButton;
+    GroupCharTypes: TGroupBox;
+    CharUp: TCheckBox;
+    CharLow: TCheckBox;
+    Digit: TCheckBox;
+    SpChar: TCheckBox;
+    GroupPassword: TGroupBox;
+    PasswordString: TEdit;
+    BtnGenPassword: TButton;
   private
 
   public
@@ -16,11 +39,14 @@ type
   end;
 
 var
-  Form1: TForm1;
+  MainForm: TMainForm;
 
 implementation
 
 {$R *.lfm}
+
+{ TMainForm }
+
 
 end.
 
